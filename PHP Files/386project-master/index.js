c25f1324b87be2dc3ac93cfc0bc8ed08c8a9e942
LukeM98 <lmartin9@gulls.salisbury.edu>
2019-12-11 18:48:38 -0500

    Removed bits

--- PHP Files/386project-master/index.js
+++
@@ -1,5 +0,0 @@
-// JavaScript source code
-function addpokemon() {
-
-    window.location.href = "addpokemon.php"
-}
--- PHP Files/386project-master/login.js
+++
@@ -1,8 +0,0 @@
-function showPass(){
-  var x = document.getElementById("input");
-  if(x.type === "password"){
-    x.type = "text";
-  }else{
-    x.type = "password";
-  }
-}
